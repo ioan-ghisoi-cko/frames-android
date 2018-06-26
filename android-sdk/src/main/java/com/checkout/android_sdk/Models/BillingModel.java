f8dfdc1d58b258c5038fbd96779ff674eeeec782
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2018-06-26 14:48:35 +0100

    Add billing getters

--- android-sdk/src/main/java/com/checkout/android_sdk/Models/BillingModel.java
+++ android-sdk/src/main/java/com/checkout/android_sdk/Models/BillingModel.java
@@ -23,4 +23,32 @@ public class BillingModel {
         this.state = state;
         this.phone = phone;
     }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public PhoneModel getPhone() {
+        return phone;
+    }
 }
